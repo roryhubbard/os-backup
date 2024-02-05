794a799b700f089c57acd96e695a26d863ebc08d
Rory Hubbard <calvin.hubbard@shield.ai>
2024-02-04 17:42:02 -0800

    .config/fish

---
+++ .config/fish/conf.d/rand.fish
@@ -0,0 +1,9 @@
+set -x ARTIFACTORY_API_KEY "AKCp8pQSKyzhqFgF29VEC1Ckrr7qp1dbxkbZnQstm9NwZcnP4XqK4ZyigAr2EyKssivyFC2wH"
+set -x ARTIFACTORY_IDENT_TOKEN "cmVmdGtuOjAxOjE3MjcyNzU0NzI6aWpobnQ4Z0dIZGtTdzFzZlg1V3hjWGg1dk5R"
+set -x HMDSDK_API_URL "https://api.hmds.ai"
+set -x HMDSDK_API_BASIC_AUTH_TOKEN "ces.service@shield.ai:svc-api-ewSIe0iAVbOPexKeqQYt"
+#set -x ADO_PAT "dr75pzgz3ntipu3kvuzmleqdzg5wwc22yhdzkaa7yluphcb7i3iq"
+# tony demo 1/25/24
+set -x ADO_PAT "npaf54ylipnhoa4ujr7jjykocoowdrxx5xb7o7tw5qqhmykmumtq"
+#set -x ADO_PAT "jt5xe5m7adpcknz24xqtin6s3i7isy5u7gwlyz55rwrlq7p7onaq"
+
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source "$HOME/.autojump/share/autojump/autojump.fish"
+source /home/chub/.autojump/share/autojump/autojump.fish
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
@@ -31,5 +31,11 @@ set -x EDITOR "$VISUAL"
 #export OMP_NUM_THREADS=8
 
 alias vi="nvim"
+alias gls="git -P branch --list '*rhubbard*'"
 
 set -g fish_greeting
+
+pyenv init - | source
+
+starship init fish | source
+direnv hook fish | source
